ae7c61804293ba8081b84a25446845e5698538dd
Gagan <g.deepsingh1@gmail.com>
2025-03-29 19:56:29 -0600

    aws access

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,27 @@
 #!/bin/bash -l
 
 set -eo pipefail
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]; then
+  echo "error: AWS_ACCESS_KEY_ID is not set"
+  err=1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+  echo "error: AWS_SECRET_ACCESS_KEY is not set"
+  err=1
+fi
+
+if [ -z "$AWS_REGION" ]; then
+  echo "error: AWS_REGION is not set"
+  err=1
+fi
+
+if [ $err -eq 1 ]; then
+  exit 1
+fi
+
+if [ "$DEBUG" = "1" ]; then
+  echo "*** Enabling debug output (set -x)"
+  set -x
+fi
